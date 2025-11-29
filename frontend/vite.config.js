b9b67d720727d1f7621ac348d05873fa8a51ada5
Asha_hp <ashahp.work@gmail.com>
2025-11-29 12:27:47 +0530

    Modified vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // explicitly set base path for SPA
+  build: {
+    outDir: 'dist', // make sure build output is 'dist'
+  },
 })
