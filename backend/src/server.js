4ded2b0eac60f318d0c16d9b9e86d9752777f4af
Asha_hp <ashahp.work@gmail.com>
2025-11-29 19:21:58 +0530

    Fix incorrect Inngest import path

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,7 @@ import path from 'path';
 import cors from "cors";
 
 import { serve } from "inngest/express";
-import { inngest, functions } from "./inngest.js";
+import { inngest, functions } from "./lib/inngest.js";
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
 
