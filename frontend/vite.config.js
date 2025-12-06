8af3c63e28cb92969b9a9ffc8a3021994620ff6d
Asha_hp <ashahp.work@gmail.com>
2025-12-06 14:30:43 +0530

    frontend tools setup

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss(),],
   base: '/', // explicitly set base path for SPA
   build: {
     outDir: 'dist', // make sure build output is 'dist'
