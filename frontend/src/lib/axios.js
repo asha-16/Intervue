2411ab8d0f01cf261ff9c7374507530723db42be
Asha_hp <ashahp.work@gmail.com>
2025-12-12 12:35:52 +0530

    fixed baseurl

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,14 +1,13 @@
 import axios from "axios";
 
-// Dynamically choose API URL based on environment
 const baseURL =
   import.meta.env.MODE === "development"
     ? import.meta.env.VITE_API_URL_LOCAL
     : import.meta.env.VITE_API_URL_PROD;
 
 const axiosInstance = axios.create({
-  baseURL,
-  withCredentials: true, // sends cookies automatically
+  baseURL: `${baseURL}/api`, // <--- add /api here
+  withCredentials: true,
 });
 
 export default axiosInstance;
